8d9b15638109755825945ef357fe6f2c830a0c06
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-07-07 02:14:01 -0300

    arrumando as urls

--- bibliotech/urls.py
+++ bibliotech/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from bibliotech.views import home, sobre, contato
+from bibliotech.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- bibliotech/views.py
+++ bibliotech/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'bibliotech/home.html')
-
-def sobre(request):
-    return HttpResponse('Sobre1')
-
-def contato(request):
-    return HttpResponse('Contato1')
