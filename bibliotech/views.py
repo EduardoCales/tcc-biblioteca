f70cbcbcb4c4d5ad0fcdc8e99a14a7326a624040
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-07-08 22:58:12 -0300

    colocando dados com a funcao faker

--- bibliotech/views.py
+++ bibliotech/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from utils.bibliotech.factory import make_book
 
 def home(request):
-    return render(request, 'bibliotech/pages/home.html')
+    return render(request, 'bibliotech/pages/home.html', context= {
+        'livros': [make_book() for _ in range(10)],
+    })
 
 def livro(request, id):
-    return render(request, 'bibliotech/pages/livro-view.html')
+    return render(request, 'bibliotech/pages/livro-view.html', context={
+        'livro': make_book(),
+    })
